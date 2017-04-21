65cf0ce1d0d27dac1a34c24f35aad11b98b41f25
t4ss5t <t4ss5t@users.noreply.github.com>
2017-04-21 16:04:37 +0200

    Modifed with new MQTT topic

--- aws_publish.js
+++ aws_publish.js
@@ -27,21 +27,11 @@ port.on('data', function (data) {
   	var timestamp = Date.now();
   	if( obj && obj.sensor ) {
 		obj.timestamp = timestamp;
-		//device.publish('weather_shield_measure_data', JSON.stringify(obj));
-		device.publish('weather_shield_measure_data/' + obj.sensor, JSON.stringify(obj));
+		//device.publish('mod_1023_measure_data', JSON.stringify(obj));
+		device.publish('mod_1023_measure_data/' + obj.sensor, JSON.stringify(obj));
   	}
   } catch(e) {}
 });
-
-
-/*
-device
-  .on('connect', function() {
-    console.log('connect');
-    device.subscribe('topic_1');
-    device.publish('topic_2', JSON.stringify({ test_data: 1}));
-    });
-*/
 
 device
   .on('message', function(topic, payload) {
