1500dee415ffe5e86c40579bebb9769fc9003af9
t4ss5t <t4ss5t@users.noreply.github.com>
2017-04-22 09:38:13 +0200

    Update

--- aws_publish_mod_1023.js
+++ aws_publish_mod_1023.js
@@ -1,1 +1,39 @@
+var awsIot = require('aws-iot-device-sdk');
+var SerialPort = require("serialport");
 
+var device = awsIot.device({
+   keyPath: 'private.pem.key',
+  certPath: 'certificate.pem.crt',
+    caPath: 'root-CA.pem',
+  clientId: '123',
+    region: 'eu-west-1'
+});
+
+var port = new SerialPort("/dev/ttyACM0", {
+  baudRate: 115200,
+  //autoOpen: false,
+  parser: SerialPort.parsers.readline('\n')
+});
+
+port.on('error', function(err) {
+  console.log('Error: ', err.message);
+})
+
+port.on('data', function (data) {
+  //console.log(data);
+  try {
+  	var obj = JSON.parse( (data).replace(/'/g,'"') );
+  	console.log(obj);
+  	var timestamp = Date.now();
+  	if( obj && obj.sensor ) {
+		obj.timestamp = timestamp;
+		//device.publish('mod_1023_measure_data', JSON.stringify(obj));
+		device.publish('mod_1023_measure_data/' + obj.sensor, JSON.stringify(obj));
+  	}
+  } catch(e) {}
+});
+
+device
+  .on('message', function(topic, payload) {
+    console.log('message', topic, payload.toString());
+  });
