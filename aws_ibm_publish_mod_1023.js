11861898b14fb67d0012166620175b14a83d5324
TS <github@services-tho-schmidt.de>
2017-04-23 16:13:29 +0200

    bugfix

--- aws_ibm_publish_mod_1023.js
+++ aws_ibm_publish_mod_1023.js
@@ -19,7 +19,7 @@ awsDevice.on('message', function(topic, payload) {
   console.log('message', topic, payload.toString());
 });
 
-var ibmDevice = new Client.IotfDevice(credentialsIbm);
+var ibmDevice = new ibmIot.IotfDevice(credentialsIbm);
 
 ibmDevice.log.setLevel('info');
 ibmDevice.connect();
